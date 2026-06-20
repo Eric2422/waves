63ad202886cc07a5e3f6be5ce0abc6fda602b65f
Eric Zhang <ericbkzhang@gmail.com>
2026-06-20 09:02:04 -0400

    Remove variable assignment from doc examples
    Variable assignment provided no helpful information on usage.
    Showing the input and returned value should be sufficient.
    Also, assigning to a variable means that I have to come up with a
    variable name.

--- src/array_math.rs
+++ src/array_math.rs
@@ -8,7 +8,7 @@
 ///
 /// ```rust
 /// // Returns [4, 4, 4].
-/// let array_sum = add_arrays([1, 2, 3], [3, 2, 1]);
+/// add_arrays([1, 2, 3], [3, 2, 1])
 /// ```
 pub fn add_arrays(array1: [f64; 3], array2: [f64; 3]) -> [f64; 3] {
     [
@@ -25,7 +25,7 @@ pub fn add_arrays(array1: [f64; 3], array2: [f64; 3]) -> [f64; 3] {
 ///
 /// ```rust
 /// // Returns [10, 20, 30].
-/// let new_array = multiply_array_by_scalar([1, 2, 3], 10);
+/// multiply_array_by_scalar([1, 2, 3], 10)
 /// ```
 pub fn multiply_array_by_scalar(array: [f64; 3], scalar: f64) -> [f64; 3] {
     [array[0] * scalar, array[1] * scalar, array[2] * scalar]
@@ -53,7 +53,7 @@ pub fn divide_array_by_scalar(array: [f64; 3], scalar: f64) -> [f64; 3] {
 ///
 /// ```rust
 /// // Returns approximately 5.0.
-/// let magnitude = calculate_array_magnitude([3, 4, 0]);
+/// calculate_array_magnitude([3, 4, 0])
 /// ```
 pub fn calculate_array_magnitude(array: [f64; 3]) -> f64 {
     (array[0].powi(2) + array[1].powi(2) + array[2].powi(2)).sqrt()
